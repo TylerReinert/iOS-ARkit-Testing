34e04ac8dfb8a8a6475564aaf15b0cf24b622335
Tyler Reinert <tyler.reinert94@gmail.com>
2017-09-22 18:37:06 -0500

    Add files via upload
    Changed plane postion

--- AR-Test/ViewController.swift
+++ AR-Test/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
+        // Create a new scene, where you add the objects
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
